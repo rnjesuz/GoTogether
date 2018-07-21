d89789d88b66f9e1dd001f1e2105d4da7e35eb00
rnjesuz <rn_jesuz@hotmail.com>
2018-07-21 21:50:41 +0100

    More work into the redesign of CreateEventActivity

--- app/src/main/java/app/gotogether/CreateEvent2Activity.java
+++ app/src/main/java/app/gotogether/CreateEvent2Activity.java
@@ -1,10 +1,16 @@
 package app.gotogether;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 import expandablelib.gotogether.ExpandCollapseListener;
 import expandablelib.gotogether.ExpandableLayout;
@@ -15,12 +21,55 @@ public class CreateEvent2Activity extends AppCompatActivity {
     private String parents = "Do you volunteer as a Driver?";
     private boolean isDriver = false;
     private int emptySeats = -1; // -1 = no car. >0 = how many empty seats
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event2);
 
+    }
+
+    private void DestinationAutoComplete(View view){
+
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                            .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+
+
+    }
+
+    private void StartAutoComplete(View view){
+
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                            .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+
+    }
+
+    private void DriverVolunteer(){
+        LinearLayout driverInfo = (LinearLayout) findViewById(R.id.driverInfo);
+        if(isDriver) {
+            driverInfo.setVisibility(View.GONE);
+            isDriver = true;
+        } else {
+            driverInfo.setVisibility(View.VISIBLE);
+            isDriver = false;
+        }
     }
 
 }
