2fbb43178351ee422c1b838d1e9d7fe31a203ed3
rnjesuz <11352735+rnjesuz@users.noreply.github.com>
2019-02-21 23:52:24 +0000

    Update main.py

--- server/python-functions/cluster_distance_radius/main.py
+++ server/python-functions/cluster_distance_radius/main.py
@@ -23,6 +23,31 @@ participants = {}
 
 ######################
 def cluster_distance_radius(request):
+    """
+        Calculates the best possible cluster, applying an heuristic based on percentage of route shared
+
+        The algorithm splits participants in riders and drivers
+        Then, the riders are matched with the best available driver based on the heuristic
+        Then, drivers are matched between themselves:
+            A cluster is calculated to minimize cars using a bin-packing algorithm
+            A cluster is calculated to minimize total distance using the heuristic
+            A weight function is applied to each cluster
+            f(x)=(cars_parameter*(len(x)/initial_cars))+(distance_parameter*(distance(x)/initial_distance))
+            Both functions are compared and the best cluster is chosen
+        The database is updated with the final cluster
+
+        :param request: a formatted request with values for the algorithm
+                eventUID (String): The name of the event, from which to calculate the cluster
+                cars (float): How much weight given to minimizing number of cars. Cars = 100 - distance
+                distance (float): How much weight given to minimizing total distance traveled. Distance = 100 - cars
+                optimization (boolean): Optimize the order of the riders in each car? *TODO*
+        :type request: JSON
+
+        :return: None
+
+        TODOs
+            Complete implementation of module using 'optimization' parameter
+        """
     global event_ref
     global participants
     global destination
@@ -66,17 +91,17 @@ def cluster_distance_radius(request):
         distance_results = gmaps.distance_matrix(source, destination)  # TODO this can result ZERO_RESULTS
         if p.is_driver():
             drivers.append(p.id)
-            drivers_distance[p.id] = distance_results.get(u'rows')[0].get(u'elements')[0].get(u'distance').get(u'value')
+            # drivers_distance[p.id] = distance_results.get(u'rows')[0].get(u'elements')[0].get(u'distance').get(u'value')
             cluster[p.id] = []
         else:
             riders.append(p.id)
-            riders_distance[p.id] = distance_results.get(u'rows')[0].get(u'elements')[0].get(u'distance').get(u'value')
+            # riders_distance[p.id] = distance_results.get(u'rows')[0].get(u'elements')[0].get(u'distance').get(u'value')
         participants[p.id] = p
 
     radius = 5  # Kilometers
     matches = 0
-    while len(riders_distance.keys()) > matches:
-        for driver in drivers_distance:
+    while len(riders) > matches:
+        for driver in drivers:
             participant = participants.get(driver)
             seats = participant.get_seats()
             possible_riders = riders.copy()
@@ -100,45 +125,62 @@ def cluster_distance_radius(request):
     print(u'Radial clusters: {}'.format(cluster))
 
     print('------------------------------')
-    print('Initial values.')
+    print('Calculating INITIAL values.')
     initial_cars = len(cluster)
+    print('Initial number of  cars: ' + str(initial_cars) + '.')
     initial_distance = calculate_cluster_distance(cluster)
-    print('Distance: ' + str(initial_distance))
-    print('# Cars: ' + str(initial_cars) + '.')
+    print('Total initial distance: ' + str(initial_distance))
 
     # ----------------------------------
     #
-    # f(x)=(cars_parameter*(len(x)/initial_cars))+(distance_parameter*(calculate_cluster_distance(x)/initial_distance))
+    # f(x)=(cars_parameter*(len(x)/initial_cars))+(distance_parameter*(distance(x)/initial_distance))
     #
     print('------------------------------')
-    print('Calculating cluster minimizing cars.')
+    print('Calculating cluster minimizing CARS.')
     cluster_cars = group_cells_cars(copy.deepcopy(cluster))
-    print('cluster_cars: {}'.format(cluster_cars))
     distance_cars = calculate_cluster_distance(cluster_cars)
-    print('Distance: ' + str(distance_cars))
-    print('# Cars: ' + str(len(cluster_cars)) + '.')
     f_cluster_cars = (cars_parameter * (len(cluster_cars) / initial_cars)) + \
                      (distance_parameter * (distance_cars / initial_distance))
-    print('function_cluster_cars: ' + str(f_cluster_cars) + '.')
     print('------------------------------')
-    print('Calculating cluster minimizing distance.')
-    cluster_distance = group_cells_distance(copy.deepcopy(cluster), drivers_distance, drivers)
-    print('cluster_distance: {}'.format(cluster_distance))
+    print('Calculating cluster minimizing DISTANCE.')
+    cluster_distance = group_cells_distance(copy.deepcopy(cluster), drivers_distance)
     distance_distance = calculate_cluster_distance(cluster_distance)
-    print('Distance: ' + str(distance_distance))
-    print('# Cars: ' + str(len(cluster_distance)) + '.')
     f_cluster_distance = (cars_parameter * (len(cluster_distance) / initial_cars)) + \
                          (distance_parameter * (distance_distance / initial_distance))
+    print('------------------------------')
+    print('Initial values.')
+    print("Route clusters: {}".format(cluster))
+    print('# Cars: ' + str(initial_cars) + '.')
+    print('Distance: ' + str(initial_distance))
+    print('------------------------------')
+    print('Cluster by min of cars')
+    print('Cluster: {}'.format(cluster_cars))
+    print('# of Cars: ' + str(len(cluster_cars)) + '.')
+    print('Distance: ' + str(distance_cars))
+    print('function_cluster_cars: ' + str(f_cluster_cars) + '.')
+    print('------------------------------')
+    print('Cluster by min distance')
+    print('Cluster: {}'.format(cluster_distance))
+    print('# Cars: ' + str(len(cluster_distance)) + '.')
+    print('Distance: ' + str(distance_distance))
     print('function_cluster_distance: ' + str(f_cluster_distance) + '.')
 
     if event_optimization:
         # call waypoint optimization method TODO
         pass
-    update_database(cluster_cars) if f_cluster_cars < f_cluster_distance else update_database(cluster_distance)
+    print('------------------------------')
+    if f_cluster_cars < f_cluster_distance:
+        print('Choosing f_cars')
+        print('Final Cluster: {}'.format(cluster_cars))
+        update_database(cluster_cars)
+    else:
+        print('Choosing f_distance')
+        print('Final Cluster: {}'.format(cluster_distance))
+        update_database(cluster_distance)
     # return cluster
     # return 'OK'
     # data = {'response': 'OK'}
-    # return json.dumps(data))
+    # return json.dumps(data)
 
 
 ######################
@@ -235,14 +277,39 @@ def group_cells_distance(cluster_distance, drivers_distance, drivers):
 
 ######################
 def verify_cumulative_distance(cluster, new_driver, old_driver):
-    old_distance_old_driver = calculate_cluster_distance({new_driver: cluster.get(new_driver)})
-    old_distance_new_driver = calculate_cluster_distance({old_driver: cluster.get(old_driver)})
+    """
+    Verifies if, based on the cluster, two drivers should travel together of separately
+
+    Individually calculate the travelled distance of both drivers with their passengers to the destination
+    Add them together to obtain the total distance travelled, if separate
+    Calculate the distance the new driver travels by picking up its passengers and the old_driver + riders
+    This gives the distance travelled together
+    Compare the two
+
+    :param cluster: the set with drivers and respective riders
+    :type cluster: dict
+    :param new_driver: the unique identifier of one driver. Must be present in the cluster.
+    :type new_driver: str
+    :param old_driver: the unique identifier of the other driver. Must be present on the cluster.
+    :type old_driver: str
+    :return: True if the distance travelled is shorter or equal together, False if its shorter separate.
+    :rtype bool
+    """
+    print("        Separate distance:")
+    old_distance_new_driver = calculate_cluster_distance({new_driver: cluster.get(new_driver)})
+    old_distance_old_driver = calculate_cluster_distance({old_driver: cluster.get(old_driver)})
     old_distance = old_distance_old_driver + old_distance_new_driver
+    print("        Total: " + str(old_distance))
     # TODO does the dictionary for the new_distance always old?
-    new_distance = calculate_cluster_distance({new_driver: cluster.get(new_driver)+[old_driver]+cluster.get(old_driver)})
+    print("        Joined distance:")
+    new_distance = calculate_cluster_distance(
+        {new_driver: cluster.get(new_driver) + [old_driver] + cluster.get(old_driver)})
+    print("        Total: " + str(new_distance))
     if old_distance < new_distance:
+        print('    A: Separate')
         return False
     else:
+        print('    A: Joined')
         return True
 
 
@@ -267,12 +334,13 @@ def calculate_cluster_distance(cluster):
     return total_distance
 
 
-######################
+#########################
 def group_cells_cars(cluster_cars):
     global participants
     # order cars by number of empty seats
     driver_seats = ValueSortedDict()
     driver_passengers = {}
+    print("Starting cluster: {}".format(cluster_cars))
     for driver in cluster_cars.keys():
         # get number of empty seats
         cluster_list = cluster_cars.get(driver)
@@ -280,34 +348,118 @@ def group_cells_cars(cluster_cars):
         driver_seats[driver] = participants.get(driver).get_seats() - cluster_list_length
         # get the number of passenger + the driver
         driver_passengers[driver] = cluster_list_length + 1
+    print("car OCCUPANCY: {}".format(driver_passengers))
+    print("Car VACANCY: {}".format(dict(driver_seats)))
 
     grouping = True
     while grouping:
+
+        # check if there's any cars still available to group
+        if not driver_seats:  # evaluates to true when empty
+            grouping = False  # end loop
+            continue  # exit current iteration
+
         # Run the bin packing algorithm with bins of capacity equal to that of the car with more available seats.
         #    The car with the most empty seats must not be an item of the the bin packing
         copy_driver_passengers = driver_passengers.copy()
-        del copy_driver_passengers[list(driver_seats.keys())[-1]]
+        new_driver = list(driver_seats.keys())[-1]
+        del copy_driver_passengers[new_driver]
+        driver_passengers_tuple = [(k, v) for k, v in copy_driver_passengers.items()]
+        #    Order the cars based on the heuristic
+        driver_passengers_tuple = order_by_heuristic(new_driver, driver_passengers_tuple)
+        print(driver_passengers_tuple)
         #    Calculate the bin packing solution
-        bins = binpacking.to_constant_volume(copy_driver_passengers, list(driver_seats.values())[-1])
-        # Of the bins produced by the algorithm, choose the bin with more items in it.
-        biggest_bin = max(enumerate(bins), key=lambda tup: len(tup[1]))[1]
+        available_seats = list(driver_seats.values())[-1]
+        if (not driver_passengers_tuple) == False:
+            bins = binpacking.to_constant_volume(driver_passengers_tuple, available_seats, 1, -1, available_seats + 1)
+        else:
+            del driver_seats[new_driver]
+            continue
+        print(bins)
+        # if the first position is empty it means ALL the values given are bigger than then bin size
+        # e.g. bin_size = 2 & bin_packing = [{}, {"A": 6}]
+        if not bins[0]:
+            del driver_seats[new_driver]  # so we remove this driver from cars with available seats
+            continue
+
+        possible_best_bin = {}
+        possible_best_bin_index = 0
+        for b in bins:
+            new_passengers = 0
+            for passengers in b:
+                # if the calculations had a car with more people than available seats ...
+                # e.g. bin_size = 2 & bin_packing = [{"A": 1}, {"B": 6}]
+                # bin_size = 2 & bin_packing = [{"A": 1, "B": 2}] ---> doesn't happen
+                if passengers[1] > available_seats:
+                    continue  # ... we skip it
+                new_passengers += passengers[1]
+            possible_best_bin[possible_best_bin_index] = new_passengers
+            possible_best_bin_index += 1
+        print(possible_best_bin)
+        bins_with_more_passengers = [u for u, v in possible_best_bin.items() if
+                                     int(v) >= max(possible_best_bin.values())]
+        print(bins_with_more_passengers)
+        best_bins = [bins[x] for x in bins_with_more_passengers]
+        print(best_bins)
+
+        # If multiple solutions exist...
+        if len(best_bins) > 1:
+            # ... compare them by distance...
+            bin_distances = []
+            for bin in best_bins:
+                cluster_bins = {new_driver: []}
+                for participant in bin:
+                    cluster_bins[new_driver].append(participant[0])  # picking up the driver
+                    cluster_bins[new_driver] += cluster_cars[participant[0]]  # and it's passenger
+                bin_distances.append(calculate_cluster_distance(cluster_bins))
+            # ... and pick the one with the smallest distance (if tied between several - choose any)
+            better_bin = tuple(best_bins)[bin_distances.index(min(bin_distances))]
+        else:
+            better_bin = next(iter(best_bins))
+        print(better_bin)
+        print_cluster = []
+        for i in range(0, better_bin.__len__()):
+            driver_of_bin = better_bin[i][0]
+            print_cluster += [driver_of_bin] + cluster_cars[driver_of_bin]
+        print("Best bin for {}: {}".format(new_driver, print_cluster))
+
         # Remove the grouped cars (the bin + the items) from the sorted list and from the unplaced items.
-        for driver in biggest_bin.keys():
-            del driver_seats[driver]
-            del driver_passengers[driver]
+        for driver in better_bin:
+            del driver_seats[driver[0]]
+            del driver_passengers[driver[0]]
+        # Remove the receiving driver from the sorted list and from the unplaced items.
+        del driver_seats[new_driver]
+        del driver_passengers[new_driver]
         # Update cluster. The biggest bin as passengers of the driver with more empty seats
-        for old_driver in biggest_bin:  # join cars
-            for rider in cluster_cars.get(old_driver):
-                cluster_cars[list(driver_seats.keys())[-1]].append(rider)
-            cluster_cars[list(driver_seats.keys())[-1]].append(old_driver)
+        for old_driver in better_bin:  # join cars
+            for rider in cluster_cars.get(old_driver[0]):
+                cluster_cars[new_driver].append(rider)
+            cluster_cars[new_driver].append(old_driver[0])
             # remove old car from available clusters
-            del cluster_cars[old_driver]
+            del cluster_cars[old_driver[0]]
         # Repeat the bin packing algorithm with the next emptiest car and with the remaining passenger groups,
         # until no more packing is possible.
-        if not copy_driver_passengers:  # evaluates to true when empty
-            grouping = False  # end loop
 
+    print("Calculated cluster: {}".format(cluster_cars))
     return cluster_cars
+
+
+######################
+def order_by_heuristic(driver, driver_passengers_tuple):
+    """
+    Orders the set after calculating values based on the heuristic
+
+    Calculates the heuristic value between the driver and each element of the set
+    Adds each calculated value to the last position of each element of the set
+    Orders the set based on its stored parameter first and then the heuristic value
+
+    :param driver: the unique identifier of the driver
+    :type driver: str
+    :param driver_passengers_tuple: a list of tuples containing several participants and their seats
+    :return: the new set, ordered, with the new heuristic values
+    :rtype list of tuples
+    """
+    pass
 
 
 ######################
@@ -353,19 +505,29 @@ def group_cells(cluster, drivers_distance):
 
 #########################
 def update_database(cluster):
+    """
+    Updates the database with the given cluster
+
+    :param cluster: the cluster of drivers and matched riders
+    :type dict
+    :return: None
+    """
     event_ref.update({u'completed': True})
-    event_ref.update({u'cluster': firestore.DELETE_FIELD}) # make sure there is no old field (SHOULDN'T HAPPEN)
+
+    event_ref.update({u'cluster': firestore.DELETE_FIELD})  # make sure there is no old field (SHOULDN'T HAPPEN)
     for driver in cluster.keys():
         cluster_riders = []
         for rider in cluster.get(driver):
             rider_ref = db.collection(u'users').document(rider)
             cluster_riders.append(rider_ref)
-        event_ref.update({u'cluster.th'+driver: cluster_riders})
+        event_ref.update({u'cluster.' + driver: cluster_riders})
 
 
 #######################
 class Participant(object):
-
+    """
+    This class is used as a representation of an event's participant
+    """
     id = None
     seats = 0
 
@@ -373,6 +535,12 @@ class Participant(object):
         self.__dict__.update(fields)
 
     def to_dict(self):
+        """
+        Returns self's data as a dictionary
+
+        :return: dictionary with 'username', 'start' location information and, if 'driver' or not
+        :rtype dict
+        """
         if not self.driver:
             dictionary = {u'username': self.username, u'start': self.start, u'driver': False}
         else:
@@ -380,23 +548,52 @@ class Participant(object):
         return dictionary
 
     def is_driver(self):
+        """
+        Is the participant listed as a driver
+
+        :return: True if driver, False if not
+        :rtype bool
+        """
         if self.driver:
             return True
         else:
             return False
 
     def set_id(self, id):
+        """
+        Set the unique identifier of the participant
+
+        :param id: the unique identifier
+        :type id: str
+        :return: None
+        """
         self.id = id
 
     def set_seats(self, seats):
+        """
+        Set how many seats the participant's car has
+
+        :param seats: total seats in the car
+        :type seats: int
+        :return: None
+        """
         self.seats = seats
 
     def get_seats(self):
+        """
+        Return how many seats the participant's car has
+
+        :return: The number of seats in the car
+        :rtype int
+        """
         return int(self.seats)
 
 
 #######################
 class Event(object):
+    """
+    This class is used as a local representation of an event in the database
+    """
     def __init__(self, **fields):
         self.__dict__.update(fields)
 
